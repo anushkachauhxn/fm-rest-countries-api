4a9d41f3779946eb1990dc7a10adce11c9aa5737
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-01-27 23:03:14 +0530

    Main Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="app">
       <Header />
+
+      <div className="app__body">
+        <Main />
+      </div>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './Main.css';
+import { Button } from '@mui/material';
+
+function Main() {
+    const [menu, setMenu] = useState(false);
+    const [menuLabel, setMenuLabel] = useState("Filter by Region");
+
+    return (
+        <div className="main">
+            <div className="main__header">
+                <div className="main__search">
+                    <ion-icon name="search-sharp"></ion-icon>
+                    <input 
+                        type="search" 
+                        name="search" 
+                        id="search" 
+                        placeholder="Search for a country..." 
+                    />
+                </div>
+
+                <div className="main__filter">
+                    <Button 
+                        className="main__filterBtn" 
+                        onClick={() => setMenu(!menu)}
+                        endIcon={<ion-icon name="chevron-down-sharp"></ion-icon>}
+                    >
+                        {menuLabel}
+                    </Button>
+                    <ul onClick={() => setMenu(false)} className={`main__filterOptions ${menu && "active"}`}>
+                        <li>All</li>
+                        <li>Africa</li>
+                        <li>Americas</li>
+                        <li>Asia</li>
+                        <li>Europe</li>
+                        <li>Oceania</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="main__body">
+            </div>
+        </div>
+    );
+}
+
+export default Main;
