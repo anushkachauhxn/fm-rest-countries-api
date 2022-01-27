90ab6a231d3014cddc5ead012e8a8d647a6e15f2
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-01-27 23:31:44 +0530

    Toggle Theme Functionality Using Redux: darkmodeSlice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectDarkMode } from './features/darkmodeSlice';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 
 function App() {
+  const darkmode = useSelector(selectDarkMode);
+
   return (
-    <div className="app">
+    <div className={`app ${darkmode && "darkmode"}`}>
       <Header />
 
       <div className="app__body">
